fc3645920d3e1fc68be0cfc23173f488b1ed607b
ShashankKumarSaxena <shashanknetsol@gmail.com>
2022-03-12 20:53:40 +0530

    basic setting up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv;
+use serenity::{
+    framework::{
+        standard::{CommandGroup, CommandResult},
+        StandardFramework,
+    },
+    http::Http,
+    model::webhook::Webhook,
+    prelude::*,
+};
+use tokio;
+use tracing_subscriber;
+
+#[tokio::main]
+async fn main() -> CommandResult {
+    dotenv::dotenv().expect("Failed to load .env file!");
+
+    Ok(())
 }
