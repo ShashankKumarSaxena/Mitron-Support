4a5b088abb060bad8b7be896cffb6284a63bffad
ShashankKumarSaxena <shashanknetsol@gmail.com>
2022-03-13 12:32:21 +0530

    welcome commands

--- src/commands/welcomer/welcome.rs
+++ src/commands/welcomer/welcome.rs
@@ -5,6 +5,7 @@ use serenity::{
     prelude::*,
 };
 use sqlx::Row;
+use std::i64;
 use tracing::warn;
 
 #[command]
@@ -13,7 +14,7 @@ use tracing::warn;
 #[example("#welcome")]
 #[only_in(guilds)]
 #[required_permissions("MANAGE_GUILD")]
-async fn welcome(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+async fn welcome(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
     if msg.mention_channels.len() == 0 {
         msg.channel_id
             .send_message(&ctx.http, |m| {
@@ -50,7 +51,7 @@ async fn welcome(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult 
                     m.embed(|e| {
                         e.description(
                             format!(
-                                "Successfully set the welcome channel to <#{}>",
+                                "✅ Successfully set the welcome channel to <#{}>",
                                 channel.id.0
                             )
                             .as_str(),
@@ -63,6 +64,67 @@ async fn welcome(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult 
         }
         None => {
             warn!("[COMMAND ERROR] Guild ID not found!")
+        }
+    }
+
+    Ok(())
+}
+
+#[command("welcome-disable")]
+#[description("Disable welcome messages.")]
+#[only_in(guilds)]
+#[required_permissions("MANAGE_GUILD")]
+async fn welcome_disable(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let db = ctx
+        .data
+        .read()
+        .await
+        .get::<PgConnectionPool>()
+        .unwrap()
+        .clone();
+
+    match msg.guild_id {
+        Some(guild_id) => {
+            let cursor = sqlx::query("SELECT welcome_channel_id FROM guildconfig WHERE id = $1")
+                .bind(i64::from(guild_id))
+                .fetch_one(&db)
+                .await
+                .unwrap();
+
+            let _value = match cursor.try_get::<i64, _>("welcome_channel_id") {
+                Ok(value) => value,
+                Err(_) => {
+                    msg.channel_id
+                        .send_message(&ctx.http, |m| {
+                            m.embed(|e| {
+                                e.title("⚠️ Welcome channel not set!");
+                                e.description("Please set a welcome channel first.");
+                                e
+                            })
+                        })
+                        .await?;
+                    return Ok(());
+                }
+            };
+            
+            sqlx::query("UPDATE guildconfig SET welcome_channel_id = NULL WHERE id = $1;")
+                .bind(i64::from(guild_id))
+                .execute(&db)
+                .await?;
+            
+            msg.channel_id.send_message(&ctx.http, |m| {
+                m.embed(|e| {
+                    e.description(
+                        "✅ Successfully disabled welcome messages!",
+                    );
+                    e
+                })
+            }).await?;
+            return Ok(());
+        }
+        None => {
+            warn!("[COMMAND ERROR] Guild ID not found!");
+            return Ok(());
         }
     }
 
--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS guildconfig (
     id BIGINT,
-    welcome_channel_id BIGINT,
+    welcome_channel_id BIGINT DEFAULT NULL,
     PRIMARY KEY(id)
 );
